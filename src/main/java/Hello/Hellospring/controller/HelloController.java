b256abf23bd7bd9df5ddb0e1b36da9cfaf7b8ccc
hwanhee Lee <brightfant@gmail.com>
2023-03-14 20:25:13 +0900

    Initial commit

---
+++ src/main/java/Hello/Hellospring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package Hello.Hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "Spring:)");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
